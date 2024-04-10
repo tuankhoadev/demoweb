11c851a82118042aea354de7f14a47be544ece8c
tuankhoadev <giaphuynhtuankhoa1106@gmail.com>
2024-04-11 00:51:45 +0700

    weblaravel

---
+++ resources/views/giaodien.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Cá Nhân</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<style>
+    body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: seagreen;
+}
+
+.container {
+    max-width: 800px;
+    margin: 20px auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #ccc;
+}
+
+header h1 {
+    text-align: center;
+}
+
+section {
+    margin-bottom: 20px;
+}
+
+h2 {
+    color: #333;
+}
+
+p {
+    margin-bottom: 10px;
+}
+</style>
+<body>
+    <div class="container">
+        <header>
+            <h1>Chào mừng đến với trang cá nhân của tôi</h1>
+        </header>
+            <section>
+            <h2>Thông tin cá nhân</h2>
+            <p>Tên: Giáp Huỳnh Tuấn Khoa</p>
+            <p>Ngày sinh: 11/06/2003</p>
+            <p>Địa chỉ: 43/31 Lê Hữu Trác</p>
+            <p>Email: giaphuynhtuankhoa1106@gmail.com</p>
+            <p>Số điện thoại: 0338133849</p>
+        </section>
+        <section>
+            <h2>Giới thiệu</h2>
+            <p>Xin chào, tôi là Giáp Huỳnh Tuấn Khoa. Tôi là một sinh viên đam mê công nghệ thông tin và khoa học máy tính. Tôi luôn muốn học hỏi và phát triển bản thân trong lĩnh vực này.</p>
+        </section>
+    </div>
+</body>
+</html>
